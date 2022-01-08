7a200735b4fdc965b0140012d35486c5c72628d2
MaxGraw <max@graw-online.de>
2022-01-09 00:19:19 +0100

    Changed Templates
    Updated to latest Design System Version

--- abstract/blocks/split.py
+++ abstract/blocks/split.py
@@ -1,12 +1,14 @@
 from wagtail.core import blocks
-from .media import MediaBlock
 from wagtail_link_block.blocks import LinkBlock
+from wagtail.images.blocks import ImageChooserBlock
+from wagtailmedia.blocks import VideoChooserBlock
 
 class SplitBlock(blocks.StructBlock):
 	title = blocks.TextBlock(required=False)
 	text = blocks.TextBlock(required=False)
 	link = LinkBlock(required=False)
-	media = MediaBlock(required=False)
+	image = ImageChooserBlock(required=False)
+	video = VideoChooserBlock(required=False, icon = 'media')
 
 	class Meta:
 		abstract=True

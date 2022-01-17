88e106b65f7b06321a3a0710ea065db3be9cfcee
MaxGraw <max@graw-online.de>
2022-01-17 22:19:01 +0100

    Removed Image from BlockQuote Element

--- snippets/models.py
+++ snippets/models.py
@@ -1,74 +1,81 @@
 from django.db import models
 from wagtail.admin.edit_handlers import (
-	FieldPanel,
-	FieldRowPanel,
-	MultiFieldPanel,
+    FieldPanel,
+    FieldRowPanel,
+    MultiFieldPanel,
 )
 from wagtail.core.models import TranslatableMixin
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.models import register_snippet
 
+
 class Author(models.Model):
-	first_name = models.CharField("First name", max_length=254, blank=False)
-	last_name = models.CharField("Last name", max_length=254, blank=False)
-	
-	image = models.ForeignKey(
-		'core.FablabImage',
-		null=True,
-		blank=True,
-		on_delete=models.SET_NULL,
-		related_name='+'
-	)
+    first_name = models.CharField("First name", max_length=254, blank=False)
+    last_name = models.CharField("Last name", max_length=254, blank=False)
 
-	panels = [
-		MultiFieldPanel([
-			FieldRowPanel([
-				FieldPanel('first_name'),
-				FieldPanel('last_name'),
-			])
-		], "Name"),
-		ImageChooserPanel('image')
-	]
+    image = models.ForeignKey(
+        "core.FablabImage",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
 
-	def __str__(self):
-		return self.last_name
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("first_name"),
+                        FieldPanel("last_name"),
+                    ]
+                )
+            ],
+            "Name",
+        ),
+        ImageChooserPanel("image"),
+    ]
 
-	class Meta:
-		verbose_name = "Autor"
-		verbose_name_plural = "Autoren"
+    def __str__(self):
+        return self.last_name
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autoren"
 
 
 class DeviceCategory(TranslatableMixin):
-	name = models.CharField("Category Name", max_length=255, blank=False)
-	
-	panels = [
-		FieldPanel('name'),
-	]
+    name = models.CharField("Category Name", max_length=255, blank=False)
 
-	def __str__(self):
-		return self.name
+    panels = [
+        FieldPanel("name"),
+    ]
+
+    def __str__(self):
+        return self.name
 
 
 class ProjectCategory(TranslatableMixin):
-	name = models.CharField("Category Name", max_length=255, blank=False)
-	
-	panels = [
-		FieldPanel('name'),
-	]
+    name = models.CharField("Category Name", max_length=255, blank=False)
 
-	def __str__(self):
-		return self.name
+    panels = [
+        FieldPanel("name"),
+    ]
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(TranslatableMixin):
-	name = models.CharField("Tag Name", max_length=255, blank=False)
+    name = models.CharField("Tag Name", max_length=255, blank=False)
 
-	panels = [
-		FieldPanel('name'),
-	]
+    panels = [
+        FieldPanel("name"),
+    ]
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
 
 register_snippet(Tag)
 register_snippet(Author)

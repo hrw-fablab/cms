d70cd5caee2a67241d960009e5921d9899cb6f90
MaxGraw <max@graw-online.de>
2022-01-21 13:37:54 +0100

    Added dotenv

--- settings/dev.py
+++ settings/dev.py
@@ -1,10 +1,15 @@
 from .base import *
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#pww1#m8bx2dih#91b9(_^zpc1v+ho662&)pdrw11*3+y*u3&m"
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]

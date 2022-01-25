d49d055761d454491c3410cca339281fa1fdd7b1
MaxGraw <max@graw-online.de>
2022-01-25 23:00:41 +0100

    Removed Text and Link from Banner

--- abstract/blocks/banner.py
+++ abstract/blocks/banner.py
@@ -5,9 +5,7 @@ from wagtail_link_block.blocks import LinkBlock
 
 class BannerBlock(blocks.StructBlock):
     title = blocks.TextBlock(required=False)
-    text = blocks.TextBlock(required=False)
     image = ImageChooserBlock(required=False)
-    link = LinkBlock(required=False)
 
     class Meta:
         template = "organisms/banner.html"

935c7a735e0ca12b166c3290059396495b15ac3a
MaxGraw <max@graw-online.de>
2022-01-25 15:45:06 +0100

    Added LinkBlock to Banner

--- abstract/blocks/banner.py
+++ abstract/blocks/banner.py
@@ -1,12 +1,13 @@
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
-from wagtailmedia.blocks import VideoChooserBlock
+from wagtail_link_block.blocks import LinkBlock
 
 
 class BannerBlock(blocks.StructBlock):
     title = blocks.TextBlock(required=False)
     text = blocks.TextBlock(required=False)
     image = ImageChooserBlock(required=False)
+    link = LinkBlock(required=False)
 
     class Meta:
         template = "organisms/banner.html"

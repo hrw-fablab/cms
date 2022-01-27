1e7981c15efa84495f6ba15ecb0c369e79413521
MaxGraw <max@graw-online.de>
2022-01-27 16:21:09 +0100

    Added contain checkbox to gallery

--- abstract/blocks/gallery.py
+++ abstract/blocks/gallery.py
@@ -9,10 +9,9 @@ class GalleryCards(blocks.StreamBlock):
     video = VideoChooserBlock()
     embed = EmbedBlock()
 
-
 class GalleryBlock(blocks.StructBlock):
     title = blocks.CharBlock(required=False)
-    
+    contain = blocks.BooleanBlock(required=False, label="Cointain Aspect Ratio", help_text="Wenn z.B. von Logos das Seitenverhältnis beibehalten bleiben soll, sodass das Logo nicht abgeschnitten wird.")
     cards = GalleryCards()
 
     class Meta:

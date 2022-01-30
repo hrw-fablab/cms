befd71bcb360cebfaa97aaf842f0a2a4a860259d
MaxGraw <max@graw-online.de>
2022-01-30 15:50:05 +0100

    Updated Settings
    Removed unused local import

--- settings/dev.py
+++ settings/dev.py
@@ -10,9 +10,3 @@ SECRET_KEY = "DevelopmentKeyReplaceInProduction"
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
--- settings/production.py
+++ settings/production.py
@@ -6,9 +6,3 @@ SECRET_KEY = str(os.environ.get("SECRET_KEY"))
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
-
-
-try:
-    from .local import *
-except ImportError:
-    pass

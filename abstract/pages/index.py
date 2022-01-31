1c31be94415b3a65cd3ce90a80576fc126b8d8ad
MaxGraw <max@graw-online.de>
2022-01-31 11:07:48 +0100

    Added Heading to Index Page

--- abstract/pages/index.py
+++ abstract/pages/index.py
@@ -1,6 +1,15 @@
+from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
+
 from core.models import FablabBasePage
 
 
 class AbstractIndexPage(FablabBasePage):
+    heading = models.CharField(max_length=255, blank=True)
+
+    content_panels = FablabBasePage.content_panels + [
+        FieldPanel("heading"),
+    ]
+
     class Meta:
         abstract = True

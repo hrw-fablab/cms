1328560baf54c3d6db3f64ba52a3e13b3af4c4fc
MaxGraw <max@graw-online.de>
2022-02-03 00:15:32 +0100

    updated collection page

--- websites/qu_fablab/models/collection.py
+++ websites/qu_fablab/models/collection.py
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-=======
-from pydoc import classname
->>>>>>> Stashed changes
 from django.db import models
 
 from core.models import FablabBasePage
@@ -10,10 +6,7 @@ from django.db import models
 from wagtail.admin.edit_handlers import (
     FieldPanel,
     InlinePanel,
-<<<<<<< Updated upstream
-=======
     PageChooserPanel
->>>>>>> Stashed changes
 )
 
 from modelcluster.models import ClusterableModel
@@ -23,11 +16,8 @@ from modelcluster.fields import ParentalKey
 
 from datetime import date
 
-<<<<<<< Updated upstream
-class Link(models.Model):
-=======
+
 class Link(Orderable):
->>>>>>> Stashed changes
     url = models.URLField(max_length=255)
 
     title = models.CharField(max_length=255)
@@ -46,13 +36,6 @@ class Link(Orderable):
         FieldPanel("expire"),
     ]
 
-<<<<<<< Updated upstream
-    class Meta:
-        abstract = True
-
-class LinkCollections(Orderable, Link):
-    parent = ParentalKey("QuCollectionPage", on_delete=models.CASCADE, related_name="link")
-=======
 class Page(Orderable):
     page = models.ForeignKey(
         "wagtailcore.Page",
@@ -71,16 +54,10 @@ class Page(Orderable):
         FieldPanel("amount")
     ]
 
->>>>>>> Stashed changes
-
 class QuCollectionPage(FablabBasePage, ClusterableModel):
     template = "pages/collection.html"
     
     content_panels = FablabBasePage.content_panels + [
-<<<<<<< Updated upstream
-        InlinePanel("link", label="Link"),
-=======
         InlinePanel("collection_link", label="Link", classname="collabsible"),
         InlinePanel("collection_page", label="Page", classname="collabsible"),
->>>>>>> Stashed changes
     ]

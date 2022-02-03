33735eef9b56fb4e0cf67176c88c01c66bc9e096
MaxGraw <max@graw-online.de>
2022-02-03 13:16:57 +0100

    Updated Design System

--- websites/qu_fablab/models/__init__.py
+++ websites/qu_fablab/models/__init__.py
@@ -3,7 +3,6 @@ from .folder import QuFolderPage
 from .flex import QuFlexPage
 from .article import QuArticlePage
 from .project import QuProjectPage
-from .collection import QuCollectionPage
 
 from .index import QuIndexPage
 from .index_category import QuIndexCategoryPage

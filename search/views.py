365788fb4774773ebb7b60915cd6312cce3bafe6
MaxGraw <max@graw-online.de>
2022-02-05 22:19:27 +0100

    added Search

--- search/views.py
+++ search/views.py
@@ -1,17 +1,16 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.template.response import TemplateResponse
 
 from wagtail.core.models import Page
 from wagtail.search.models import Query
 
-
 def search(request):
     search_query = request.GET.get('query', None)
     page = request.GET.get('page', 1)
 
+
     # Search
     if search_query:
-        search_results = Page.objects.live().search(search_query)
+        search_results = Page.objects.live().specific().search(search_query)
         query = Query.get(search_query)
 
         # Record hit
@@ -19,16 +18,7 @@ def search(request):
     else:
         search_results = Page.objects.none()
 
-    # Pagination
-    paginator = Paginator(search_results, 10)
-    try:
-        search_results = paginator.page(page)
-    except PageNotAnInteger:
-        search_results = paginator.page(1)
-    except EmptyPage:
-        search_results = paginator.page(paginator.num_pages)
-
-    return TemplateResponse(request, 'search/search.html', {
+    return TemplateResponse(request, 'pages/search.html', {
         'search_query': search_query,
         'search_results': search_results,
     })

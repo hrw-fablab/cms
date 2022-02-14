108f4ed4dbf18e392f8d64147b9fba7fa8419e49
MaxGraw <max@graw-online.de>
2022-02-14 16:17:50 +0100

    reworked navigation

--- static_source/js/cms.js
+++ static_source/js/cms.js
@@ -1,20 +1,36 @@
 // Function for handling Display of Submenus
-document.addEventListener("click", (event) => {
-	if (event.target.tagName == "BUTTON" && event.target.id != "menu-button") {
-		if (getComputedStyle(event.target.nextElementSibling).display === "none") {
-			for (element of document.getElementsByClassName("submenu")) { element.classList.add("hidden") }
-			return event.target.nextElementSibling.classList.remove("hidden")
-		}
-		return event.target.nextElementSibling.classList.add("hidden")
-	}
-	for (element of document.getElementsByClassName("submenu")) { element.classList.add("hidden") }
-}, {passive: true});
+document.addEventListener(
+  "click",
+  (event) => {
+    if (event.target.tagName == "BUTTON" && event.target.id != "menu-button") {
+      if (
+        getComputedStyle(event.target.nextElementSibling).display === "none"
+      ) {
+        for (element of document.getElementsByClassName("fl-submenu")) {
+          element.classList.add("hidden");
+        }
+        return event.target.nextElementSibling.classList.remove("hidden");
+      }
+      return event.target.nextElementSibling.classList.add("hidden");
+    }
+    for (element of document.getElementsByClassName("fl-submenu")) {
+      element.classList.add("hidden");
+    }
+  },
+  { passive: true }
+);
 
 // Function to handle Display of Menu on Mobile Navigation
-document.getElementById("menu-button").addEventListener("click", (event) => {
-	if (getComputedStyle(event.target.nextElementSibling).display === "none") {
-		event.target.nextElementSibling.style.display = "flex";
-		return;
-	}
-	event.target.nextElementSibling.style.display = "none";
-}, {passive: true})
+document.getElementById("menu-button").addEventListener(
+  "click",
+  (event) => {
+    if (getComputedStyle(event.target.nextElementSibling).display === "none") {
+      event.target.nextElementSibling.style.display = "flex";
+      document.getElementById("settings").style.display = "flex";
+      return;
+    }
+    event.target.nextElementSibling.style.display = "none";
+    document.getElementById("settings").style.display = "none";
+  },
+  { passive: true }
+);

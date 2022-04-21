def86e919f24ac2e936b63bdcd7ab4e1ed54b774
MaxGraw <max@graw-online.de>
2022-04-21 10:17:19 +0200

    started rework of design system

--- abstract/blocks/grabber.py
+++ abstract/blocks/grabber.py
@@ -6,14 +6,11 @@ class GrabberBlock(blocks.StructBlock):
     title = blocks.CharBlock(required=False)
     layout = blocks.ChoiceBlock(
         choices=[
-            ("news-large", "Zeitungslayout mit einem Hauptelement"),
-            ("news-medium", "Zeitungslayout mit zwei Hauptelementen"),
-            ("extrem", "1 Element pro Reihe"),
-            ("large", "2 Elemente pro Reihe"),
-            ("medium", "3 Elemente pro Reihe"),
-            ("small", "4 Elemente pro Reihe"),
+            ("newspaper", "Zeitungslayout mit einem Hauptelement"),
+            ("magazine", "Zeitungslayout mit zwei Hauptelementen"),
+            ("line", "3 Element pro Reihe"),
         ],
-        default="news-large",
+        default="newspaper",
         help_text="Die Anzahl an Elementen in einer Horizontalen Reihe",
     )
     amount = blocks.IntegerBlock(default=5)

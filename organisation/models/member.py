61079b4e94f3e649069101deffe65a33c2ac5ec7
MaxGraw <max@graw-online.de>
2022-04-23 17:49:46 +0200

    added custom FieldPanel

--- organisation/models/member.py
+++ organisation/models/member.py
@@ -1,4 +1,5 @@
 from django.db import models
+import requests
 from wagtail.admin.edit_handlers import FieldPanel
 
 from modelcluster.fields import ParentalKey
@@ -6,14 +7,36 @@ from chooser.widgets import PersonChooser
 
 from modelcluster.models import ClusterableModel
 
+from organisation.models import Role
+from organisation.models import Project
 
-class Member(ClusterableModel, models.Model):
+
+class FilteredPanel(FieldPanel):
+    def on_form_bound(self):
+        try:
+            list = self.request.path_info.split("/")
+            id = int(list[len(list) - 2])
+            roles = Role.objects.filter(link_id = id)
+            self.form.fields["role"].queryset = roles
+        except:
+            self.form[self.field_name]
+        finally:
+            super().on_form_bound()
+
+
+class Member(ClusterableModel):
     person = models.ForeignKey(
         "organisation.Person",
         related_name="+",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
+    )
+
+    link = ParentalKey(
+        "organisation.Project",
+        on_delete=models.CASCADE,
+        related_name="related_member",
     )
 
     role = models.ForeignKey(
@@ -26,14 +49,8 @@ class Member(ClusterableModel, models.Model):
 
     panels = [
         FieldPanel("person", widget=PersonChooser, heading="Mitglied"),
-        FieldPanel("role"),
+        FilteredPanel("role"),
     ]
-
-    link = ParentalKey(
-        "organisation.Project",
-        on_delete=models.CASCADE,
-        related_name="related_member",
-    )
 
     class Meta:
         verbose_name = "Mitglied"

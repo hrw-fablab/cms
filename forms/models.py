33122e49e3f062005b2e1076263d318ebca9185b
MaxGraw <max@graw-online.de>
2022-05-08 00:55:19 +0200

    changed forms

--- forms/models.py
+++ forms/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import (
     FieldPanel,
-    FieldRowPanel,
     InlinePanel,
     MultiFieldPanel,
 )
@@ -15,21 +14,15 @@ class FormField(AbstractFormField):
 
 
 class FormPage(AbstractEmailForm):
-    intro = RichTextField(blank=True)
     thank_you_text = RichTextField(blank=True)
 
     content_panels = AbstractEmailForm.content_panels + [
-        FieldPanel("intro", classname="full"),
         InlinePanel("form_fields", label="Form fields"),
-        FieldPanel("thank_you_text", classname="full"),
+        FieldPanel("thank_you_text"),
         MultiFieldPanel(
             [
-                FieldRowPanel(
-                    [
-                        FieldPanel("from_address", classname="col6"),
-                        FieldPanel("to_address", classname="col6"),
-                    ]
-                ),
+                FieldPanel("from_address"),
+                FieldPanel("to_address"),
                 FieldPanel("subject"),
             ],
             "Email",

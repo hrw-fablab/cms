567fb56e7d5d70c1c933ac5c5c7a3a9c5b1de561
MaxGraw <max@graw-online.de>
2022-05-08 18:20:07 +0200

    Update dev.py

--- settings/dev.py
+++ settings/dev.py
@@ -6,7 +6,5 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "DevelopmentKeyReplaceInProduction"
 
-SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
-
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]

0ea04d3fbacb1db3c3280bcaa0d87ac010a43799
MaxGraw <max@graw-online.de>
2022-05-11 10:01:01 +0200

    removed fl- syntax for css styling

--- design/js/cms.js
+++ design/js/cms.js
@@ -3,17 +3,15 @@ document.addEventListener(
   "click",
   (event) => {
     if (event.target.tagName == "BUTTON" && event.target.id != "menu-button") {
-      if (
-        getComputedStyle(event.target.nextElementSibling).display === "none"
-      ) {
-        for (element of document.getElementsByClassName("fl-submenu")) {
+      if (getComputedStyle(event.target.nextElementSibling).display === "none") {
+        for (element of document.getElementsByClassName("submenu")) {
           element.classList.add("hidden");
         }
         return event.target.nextElementSibling.classList.remove("hidden");
       }
       return event.target.nextElementSibling.classList.add("hidden");
     }
-    for (element of document.getElementsByClassName("fl-submenu")) {
+    for (element of document.getElementsByClassName("submenu")) {
       element.classList.add("hidden");
     }
   },

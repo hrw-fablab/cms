5a55c31584e4d023d60af39eb158de96b45b33a9
Max Graw <max@graw-online.de>
2022-07-22 23:49:17 +0200

    Revert "added calender logic improvements"
    This reverts commit 867c0c0d88673d9d6b831fc97ceb0646cdee25d4.

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ import datetime
 from calendar import monthrange
 from organisation.models import Event
 
+
 def get_events(request):
     body = json.loads(request.body)
     events = []
@@ -16,52 +17,35 @@ def get_events(request):
             events.append(element)
 
     for element in events:
-        expections = []
-        for expection in element.related_expection.all():
-            expections.append(
+        if element.repeat == "0":
+            days[element.day - 1].append(
                 {
-                    "start": {
-                        "year": expection.start.strftime("%Y"),
-                        "month": expection.start.strftime("%m"),
-                        "day": expection.start.strftime("%d")
-                    },
-                    "end": {
-                        "year": expection.end.strftime("%Y"),
-                        "month": expection.end.strftime("%m"),
-                        "day": expection.end.strftime("%d")
-                    }
+                    "title": element.title,
+                    "adress": element.adress,
+                    "length": element.length,
+                    "timeStart": element.timeStart,
+                    "timeEnd": element.timeEnd,
+                    "day": element.day,
+                    "description": element.description,
                 }
             )
-        days.append(
-            {
-                "title": element.title,
-                "adress": element.adress,
-                "link": element.link,
-                "link_text": element.link_text,
-                "length": element.length,
-                "timeStart": element.timeStart,
-                "timeEnd": element.timeEnd,
-                "day": element.day,
-                "month": body["month"],
-                "year": body["year"],
-                "start": element.start.weekday(),
-                "repeat": element.repeat,
-                "description": element.description,
-                "category": element.category,
-                "expections": expections,
-            }
-        )
+        else:
+            for day in range(days_count):
+                weekday = element.start.weekday()
+                current_date = datetime.date(body["year"], body["month"], day + 1)
+                if weekday == current_date.weekday():
+                    days[day].append(
+                        {
+                            "title": element.title,
+                            "adress": element.adress,
+                            "length": element.length,
+                            "timeStart": element.timeStart,
+                            "timeEnd": element.timeEnd,
+                            "day": day,
+                            "description": element.description,
+                        }
+                    )
 
-    days.sort(key=lambda x: x["length"])
-
-    result = {
-        "year": body["year"],
-        "month": body["month"],
-        "days": days_count,
-        "index": date.weekday(),
-        "events": days,
-    }
-
-    data = json.dumps(result)
+    data = json.dumps(days)
 
     return JsonResponse(data, safe=False)

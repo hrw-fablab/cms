4ceabab43f9ce2c6bea0f669574b716f6e0909cd
Max Graw <max@graw-online.de>
2022-09-23 13:41:15 +0200

    update styling

--- search/views.py
+++ search/views.py
@@ -2,13 +2,11 @@ from wagtail.models import Page
 from wagtail.search.models import Query
 from django.http import JsonResponse
 
-import json
-
 def search_json(request):
     search_query = request.GET.get("query", None)
 
     if search_query:
-        search_results = Page.objects.live().search(search_query)
+        search_results = Page.objects.live().filter(locale_id=1).search(search_query)
         query = Query.get(search_query)
 
         query.add_hit()
@@ -17,6 +15,7 @@ def search_json(request):
 
     for element in search_results:
         search_json.append({
-            "title": element.title
+            "title": element.title,
+            "url": element.url
         })
-    return JsonResponse(search_json, safe=False)
+    return JsonResponse(search_json[0:5], safe=False)

8235c330e5f00484b2a2142c013b1a731783d513
Max Graw <max@graw-online.de>
2022-09-23 23:07:41 +0200

    code cleanup

--- design/js/cms.js
+++ design/js/cms.js
@@ -55,3 +55,52 @@ const cardsElements = document
 const iframeElements = document
   .querySelectorAll("iframe")
   .forEach((x) => mediaObserver.observe(x));
+
+function debounce(func, delay = 250) {
+  let timerId;
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+const search = document.getElementById("search");
+const results = document.getElementById("results");
+
+const input = async (event) => {
+  resetResults();
+  if (event.target.value.length <= 1) {
+    results.style.display = "none";
+    return;
+  }
+  const query = event.target.value;
+  const res = await fetch(
+    `${window.location.origin}/search_json/?${new URLSearchParams({
+      query,
+    }).toString()}`
+  );
+  const data = await res.json();
+  if (data.length != 0) {
+    displayResults(data);
+  } else {
+    results.style.display = "none";
+  }
+};
+
+const displayResults = (data) => {
+  results.style.display = "flex";
+  data.map((item) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <a href=${item.url}>${item.title}</a>
+      `;
+    results.appendChild(li);
+  });
+};
+
+const resetResults = () => {
+  results.innerHTML = "";
+};
+
+search.addEventListener("input", debounce(input));

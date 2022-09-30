76056f06f46cda679d2494b2969bfe28c97f5362
Max Graw <max@graw-online.de>
2022-09-30 14:13:25 +0200

    update navigation logic

--- design/js/cms.js
+++ design/js/cms.js
@@ -1,35 +1,9 @@
-// Function for handling Display of Submenus
-document.addEventListener(
-  "click",
-  (event) => {
-    if (
-      event.target.dataset.type == "menu" &&
-      event.target.id != "menu-button" &&
-      event.target.nextElementSibling.classList.contains("hidden")
-    ) {
-      for (element of document.querySelectorAll('[data-menu="submenu"]')) {
-        element.classList.add("hidden");
-      }
-      return event.target.nextElementSibling.classList.toggle("hidden");
-    }
-    for (element of document.querySelectorAll('[data-menu="submenu"]')) {
-      element.classList.add("hidden");
-    }
-  },
-  { passive: true }
-);
-
 // Function to handle Display of Menu on Mobile Navigation
 document.getElementById("menu-button").addEventListener(
   "click",
   (event) => {
-    if (getComputedStyle(event.target.nextElementSibling).display === "none") {
-      event.target.nextElementSibling.style.display = "flex";
-      document.getElementById("form-search").style.display = "flex";
-      return;
-    }
-    event.target.nextElementSibling.style.display = "none";
-    document.getElementById("form-search").style.display = "none";
+    event.target.nextElementSibling.classList.toggle("visible");
+    document.getElementById("form-search").classList.toggle("visible");
   },
   { passive: true }
 );

bde7c89cb2b7a95ce62756d55bbabae2b2447f17
Max Graw <max@graw-online.de>
2022-11-13 13:19:09 +0100

    added video controls

--- design/js/cms.js
+++ design/js/cms.js
@@ -1,88 +1,116 @@
-window.onload = addAutoplay()
+window.onload = addAutoplay();
 
 function addAutoplay() {
   try {
-    const video = document.getElementById('hero-video')
+    const video = document.getElementById("hero-video");
+    const control = document.getElementById("control");
+    const play = document.getElementById("play");
+    const pause = document.getElementById("pause");
+    control.style.opacity = "1";
     if (window.innerWidth > 992) {
-      video.removeAttribute('controls', '')
-      video.setAttribute('autoplay', '')
+      pause.style.display = "block";
+      play.style.display = "none";
+      video.removeAttribute("controls", "");
+      video.setAttribute("autoplay", "");
     }
   } catch (err) {}
 }
 
+document.getElementById("control").addEventListener("click", () => {
+  const video = document.getElementById("hero-video");
+  const play = document.getElementById("play");
+  const pause = document.getElementById("pause");
+  const control = document.getElementById("control");
+  control.style.opacity = "";
+  if (video.paused) {
+    pause.style.display = "block";
+    play.style.display = "none";
+    video.play();
+    return;
+  }
+
+  pause.style.display = "none";
+  play.style.display = "block";
+  video.pause();
+});
+
 // Function to handle Display of Menu on Mobile Navigation
-document.getElementById('menu-button').addEventListener(
-  'click',
+document.getElementById("menu-button").addEventListener(
+  "click",
   (event) => {
-    event.target.nextElementSibling.classList.toggle('visible')
-    document.getElementById('form-search').classList.toggle('visible')
+    event.target.nextElementSibling.classList.toggle("visible");
+    document.getElementById("form-search").classList.toggle("visible");
   },
   { passive: true }
-)
+);
 
 const callback = (entries, observer) => {
   entries.forEach((entry, index) => {
     if (entry.isIntersecting) {
       setTimeout(() => {
-        entry.target.classList.add('fade-in')
-        observer.unobserve(entry.target)
-      }, index * 100)
+        entry.target.classList.add("fade-in");
+        observer.unobserve(entry.target);
+      }, index * 100);
     }
-  })
-}
+  });
+};
 
-const cardObserver = new IntersectionObserver(callback, {})
-const mediaObserver = new IntersectionObserver(callback, {})
+const cardObserver = new IntersectionObserver(callback, {});
+const mediaObserver = new IntersectionObserver(callback, {});
 
-const cardsElements = document.querySelectorAll('.card').forEach((x) => cardObserver.observe(x))
+const cardsElements = document
+  .querySelectorAll(".card")
+  .forEach((x) => cardObserver.observe(x));
 
-const iframeElements = document.querySelectorAll('iframe').forEach((x) => mediaObserver.observe(x))
+const iframeElements = document
+  .querySelectorAll("iframe")
+  .forEach((x) => mediaObserver.observe(x));
 
 function debounce(func, delay = 250) {
-  let timerId
+  let timerId;
   return (...args) => {
-    clearTimeout(timerId)
+    clearTimeout(timerId);
     timerId = setTimeout(() => {
-      func.apply(this, args)
-    }, delay)
-  }
+      func.apply(this, args);
+    }, delay);
+  };
 }
-const search = document.getElementById('search')
-const results = document.getElementById('results')
+const search = document.getElementById("search");
+const results = document.getElementById("results");
 
 const input = async (event) => {
-  resetResults()
+  resetResults();
   if (event.target.value.length <= 2) {
-    results.style.display = 'none'
-    return
+    results.style.display = "none";
+    return;
   }
-  const query = event.target.value
+  const query = event.target.value;
   const res = await fetch(
     `${window.location.origin}/search_json/?${new URLSearchParams({
-      query
+      query,
     }).toString()}`
-  )
-  const data = await res.json()
+  );
+  const data = await res.json();
   if (data.length != 0) {
-    displayResults(data)
+    displayResults(data);
   } else {
-    results.style.display = 'none'
+    results.style.display = "none";
   }
-}
+};
 
 const displayResults = (data) => {
-  results.style.display = 'flex'
+  results.style.display = "flex";
   data.map((item) => {
-    const li = document.createElement('li')
+    const li = document.createElement("li");
     li.innerHTML = `
       <a href=${item.url}>${item.title}</a>
-      `
-    results.appendChild(li)
-  })
-}
+      `;
+    results.appendChild(li);
+  });
+};
 
 const resetResults = () => {
-  results.innerHTML = ''
-}
+  results.innerHTML = "";
+};
 
-search.addEventListener('input', debounce(input))
+search.addEventListener("input", debounce(input));

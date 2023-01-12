b3c368c14a704c16403118eed47a640f66a9c222
Max Graw <max@graw-online.de>
2023-01-12 15:45:37 +0100

    added card logic

--- abstract/blocks/card.py
+++ abstract/blocks/card.py
@@ -7,6 +7,14 @@ class CardBlock(blocks.StructBlock):
     image = ImageChooserBlock(required=False)
     text = blocks.TextBlock(required=False)
     page = blocks.PageChooserBlock(required=False)
+    link = blocks.URLBlock(
+        required=False,
+        help_text="Wenn die Seite extern ist oder es sich um ein Dokument handelt. Muss die url angeben werden",
+    )
+    new_tab = blocks.BooleanBlock(
+        required=False,
+        help_text="Wenn die neue Seite in einen neuen Tab geöffnet werden soll",
+    )
 
     class Meta:
         template = "templates/card.html"

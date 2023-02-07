22ed47450c10a72f60c614613cddc33d525355e4
Max Graw <max@graw-online.de>
2023-02-07 19:09:05 +0100

    component

--- settings/production.py
+++ settings/production.py
@@ -6,14 +6,3 @@ SECRET_KEY = str(os.environ.get("SECRET_KEY"))
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
-    }
-}

ce2015a3fb86e34b4e517c98029927fe2923fabf
Max Graw <35422034+maxgraw@users.noreply.github.com>
2023-02-12 16:07:51 +0100

    person ui (#135)
    * started
    
    * small updates

---
+++ fablab_components/components/person/index.py
@@ -0,0 +1,20 @@
+from django_components import component
+
+
+@component.register("person")
+class Person(component.Component):
+    template_name = "person/index.html"
+
+    def get_context_data(self, title, firstname, lastname, image, link, area):
+        return {
+            "title": title,
+            "firstname": firstname,
+            "lastname": lastname,
+            "image": image,
+            "link": link,
+            "area": area,
+        }
+
+    class Media:
+        css = "person/index.css"
+        js = "person/index.js"

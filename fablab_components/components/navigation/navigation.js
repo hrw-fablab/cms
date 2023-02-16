5ea0d50dda952a15e8d1246a33ea928c264bf8e5
Max Graw <max@graw-online.de>
2023-02-16 17:16:29 +0100

    update navigation

--- fablab_components/components/navigation/navigation.js
+++ fablab_components/components/navigation/navigation.js
@@ -1,6 +1,8 @@
+const button = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
+const body = document.getElementById("body");
 
 button.addEventListener("click", (event) => {
   menu.classList.toggle("visible");
-  document.getElementById("form-search").classList.toggle("visible");
+  body.classList.toggle("overflow");
 });

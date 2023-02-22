fb038ec82e557754ae8edceb1f31c28f4ba238ea
Max Graw <max@graw-online.de>
2023-02-22 22:06:02 +0100

    Update base.py

--- settings/base.py
+++ settings/base.py
@@ -238,7 +238,7 @@ CACHES = {
     }
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_USE_TLS = True
 EMAIL_PORT = os.environ.get("EMAIL_PORT")

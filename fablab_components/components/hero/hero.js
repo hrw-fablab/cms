131d8b56f0f9db05e39eb0c847381b69372142ea
Max Graw <max@graw-online.de>
2023-03-19 11:47:02 +0100

    update hero logic

--- fablab_components/components/hero/hero.js
+++ fablab_components/components/hero/hero.js
@@ -4,6 +4,7 @@ const play = document.getElementById("play");
 const pause = document.getElementById("pause");
 
 document.getElementById("control").addEventListener("click", () => {
+  play.classList.remove("visible");
   if (video.paused) {
     play.classList.add("hidden");
     pause.classList.remove("hidden");

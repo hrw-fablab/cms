7acaf074c4d611c4bc40453ce65242dab97f1989
Max Graw <max@graw-online.de>
2023-03-20 17:38:33 +0100

    Update navigation.js

--- fablab_components/components/navigation/navigation.js
+++ fablab_components/components/navigation/navigation.js
@@ -5,8 +5,24 @@ const body = document.getElementById("body");
 
 const bottom = document.getElementById("bottom");
 
+const details = menu.getElementsByTagName("details");
+
 button.addEventListener("click", (event) => {
   button.classList.toggle("close");
   bottom.classList.toggle("visible");
   body.classList.toggle("overflow");
+});
+
+document.addEventListener("click", (event) => {
+  const target = event.target.parentNode;
+
+  if (target.open) {
+    target.setAttribute("open", "");
+  }
+
+  for (let detail of details) {
+    if (detail !== target) {
+      detail.removeAttribute("open");
+    }
+  }
 });

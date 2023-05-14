173f388d3898ae5d4d0ebb6fe6119435a695c1c0
Max Graw <max@graw-online.de>
2023-05-14 21:57:12 +0200

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 poetry install
-poetry run python manage.py migrate --settings=settings.production
+poetry run python manage.py migrate --settings=cms.settings.production
 poetry run python manage.py collectstatic --noinput
 touch tmp/restart.txt

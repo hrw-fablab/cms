a1a741cd62a94e86800b5e3134a1986d0c68f60c
Max Graw <35422034+maxgraw@users.noreply.github.com>
2023-05-19 17:43:04 +0200

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 exec >logfile.txt 2>&1
-~/.local/share/pypoetry/venv/bin/poetry install
 poetry install
 poetry run python manage.py migrate --settings=cms.settings.production
 poetry run python manage.py collectstatic --noinput

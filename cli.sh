64c6ec64369afecb32d3d8a62395dea18f3c4563
Max Graw <35422034+maxgraw@users.noreply.github.com>
2023-05-19 17:50:03 +0200

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 exec >logfile.txt 2>&1
-poetry install
-poetry run python manage.py migrate --settings=cms.settings.production
-poetry run python manage.py collectstatic --noinput
+~/.local/bin/poetry install
+~/.local/bin/poetry run python manage.py migrate --settings=cms.settings.production
+~/.local/bin/poetry run python manage.py collectstatic --noinput
 touch tmp/restart.txt
